5c11d61c3e0b40d0706b98a5f8ccea3fb438f4d7
Mario Sanchez Carrion <mariosc@gmail.com>
2019-08-17 22:49:19 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,19 +3,16 @@ from flask_session import Session
 from flask import Flask, render_template, redirect, request, session, jsonify
 from datetime import datetime
 
-
-# Creates a connection to the database
-db = SQL ( "sqlite:///data.db" )
-
 # # Instantiate Flask object named app
 app = Flask(__name__)
-
 
 # # Configure sessions
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Creates a connection to the database
+db = SQL ( "sqlite:///data.db" )
 
 @app.route("/")
 def index():
